39365438f0c014ed2ac0a58f82337530c2db5165
oso7865-ship-it <oso7865@gmail.com>
2026-07-23 17:32:47 +0900

    260723 - ApiCustomResponse, Swagger 추가

---
+++ src/main/java/com/meerkatgramv2auth/global/config/openapi/CustomApiResponse.java
@@ -0,0 +1,17 @@
+package com.meerkatgramv2auth.global.config.openapi;
+
+
+import com.meerkatgramv2auth.global.response.constant.CustomResponseCode;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponse(responseCode = "200", description = "SUCCESS")
+public @interface CustomApiResponse {
+    CustomResponseCode[] value();
+}
